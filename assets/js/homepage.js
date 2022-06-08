9acd265af1347fa06bc34beedb4cec24f1494163
Kevin Reid <kevinnivek@me.com>
2022-06-08 12:38:35 -0400

    added languages feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
@@ -90,5 +90,34 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeatureRepos = function(language) {
+ var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+ fetch(apiUrl).then(function(response) {
+     if(response.ok) {
+         response.json().then(function(data){
+         displayRepos(data.items, language);
+         });
+        } else {
+         alert('Error: Github User not found');
+     }
+ });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+        console.log(language);
+
+    if (language) {
+        getFeatureRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+// add click event listener to language buttons
+languageButtonsEl.addEventListener("click", buttonClickHandler);
